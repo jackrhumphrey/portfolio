d9b87f84f6d2a4b2b9225428f095b4319694b3bd
jackrhumphrey <jack.humphrey@xtra.co.nz>
2020-08-07 18:34:16 +1200

    update

--- src/components/About.js
+++ src/components/About.js
@@ -3,27 +3,20 @@ import React, { Component } from "react";
 class About extends Component {
   render() {
     return (
-      <div>
+      <div className="about">
         <p>
           My name is Jack Humphrey and I am a software developer based in
-          Wellington, New Zealand. I have a particular interest in front end web
-          development, with a focus on usability and simplicity of design.
-        </p>
-        <p>
-          Outside of development, my interests include painting and
-          illustration. I am a keen pianist and hold a DipABRSM in piano
-          performance. I also enjoy creating my own music. My bachelors degree
-          was in Mathematics.
+          Wellington, New Zealand. I have a focus on front end web development,
+          and enjoy bringing ideas to life.
         </p>
         <p>
           I hold a Masters degree in Software Development from the Wellington
-          ICT Graduate School, where I learned development, as well as
-          communication skills as a developer. Since then, I have been working
-          on various projects, including client work. My preferred tech stack
-          uses React.
+          ICT Graduate School, as well as a BA in Mathematics from Victoria
+          University of Wellington.
         </p>
         <p>
-          I also tutor in computer science, and work as a piano accompanist.
+          My interests include art and illustration. I also am a keen pianist
+          and hold a DipABRSM in piano performance.
         </p>
       </div>
     );
