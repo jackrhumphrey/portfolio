188397b321d381b9acab524aad9ab1e7c3876899
jackrhumphrey <jack.humphrey@xtra.co.nz>
2021-06-03 05:20:23 +1200

    formatting

--- src/components/Weave.js
+++ src/components/Weave.js
@@ -1,6 +1,6 @@
 import "./Weave.css";
 import React, { Component } from "react";
-import weave1 from "./Weave1.png";
+import weave4 from "./Weave4.png";
 import weave2 from "./Weave2.png";
 import weave3 from "./Weave3.png";
 import { withRouter } from "react-router-dom";
@@ -8,24 +8,23 @@ import { withRouter } from "react-router-dom";
 class Weave extends Component {
     render() {
         return (
-            <div class="weave">
-                <div><div>
-                    <img src={weave1} />
-                </div></div>
-                <div><div>
-                    <img src={weave2} />
-                </div></div>
-                <div><div>
-                    <img src={weave3} />
-                </div></div>
-                <div>
-                    <p>
-                        Screenshots of Weave Talent web app from Startup Weekend Wellington 2020.</p>
-                    <p>
-                        Result: third place team.
-                    </p>
+            <>
+                <img src={weave4} style={{ maxWidth: "100%", marginBottom: "25px" }} />
+                <div class="weave">
+                    <div><div>
+                        <img src={weave2} style={{ maxWidth: "100%", marginBottom: "25px" }} />
+                    </div></div>
+                    <div><div>
+                        <img src={weave3} />
+                    </div></div>
                 </div>
-            </div>
+                <p>
+                    Screenshots of Weave Talent web app from Startup Weekend Wellington 2020.
+                </p>
+                <p>
+                    Result: third place team.
+                </p>
+            </>
         );
     }
 }
